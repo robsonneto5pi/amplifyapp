dfcac7dcb15b6bf489f1ce24e492008373476830
robsonneto5pi <robsonneto5@gmail.com>
2021-03-18 22:46:22 -0300

    Atualizar a configuração do site
    Atualize o arquivo /js/config.js na implantação do site para incluir o URL de invocação do estágio que você acabou de criar. Você deve copiar o URL de invocação diretamente da parte superior da página do editor de estágio no console do Amazon API Gateway e colá-lo no chave _config.api.invokeUrl do arquivo /js/config.js dos seus sites. Ao atualizar o arquivo de configuração, certifique-se de que ele ainda contenha as atualizações que você fez no módulo anterior para o grupo de usuários do Cognito.

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'sa-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9a3a7lwtm3.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
